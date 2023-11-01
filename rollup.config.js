0f6f47969c735007e13866013afe60f8916093c4
Elizaveta Vorobyeva <vorobeva.ed@it-o.ru>
2023-11-01 11:21:46 +0300

    init project

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import css from 'rollup-plugin-import-css'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+
+export default {
+    input: 'src/main.js',
+    output: {
+        dir: 'dist',
+        format: 'iife',
+    },
+    plugins: [css(), nodeResolve()],
+}
